1dae42f2dfd56cd60df7709ea916d0257c39d15f
MarksPro <marcosfsprivado@gmail.com>
2019-08-13 15:54:34 -0300

    Adicionando style global

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+      <GlobalStyle/>
+    </>
   );
 }
 
---
+++ src/styles/global.js
@@ -0,0 +1,19 @@
+import {createGlobalStyle} from 'styled-components';
+
+export default createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    outline: 0;
+    box-sizing: border-box;
+  }
+
+  body, html, #root {
+    height: 100%;
+    font-family: 'Montserrat', sans-serif;
+    color: #fff;
+    background: #181818;
+    text-rendering: optimizeLegibility !important;
+    -webkit-font-smoothing: antialiased !important;
+  }
+`;
