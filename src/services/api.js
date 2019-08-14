4ec95f118364cebe5ef534f05193721280dd21c5
MarksPro <marcosfsprivado@gmail.com>
2019-08-14 17:10:31 -0300

    Passando json-server para github

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:3001'
+  baseURL: 'https://my-json-server.typicode.com/markspro/dbreactfy'
 });
 
 export default API;
